4be72848cbaf03422b2ab691c1d1951a61d1b1c5
Remko Jantzen <remko.jantzen@episerver.com>
2020-12-15 17:27:47 +0000

    Fixed adding of content to the site

--- src/Foundation/ClientResources/Scripts/ContentEditing/CreateContent.js
+++ src/Foundation/ClientResources/Scripts/ContentEditing/CreateContent.js
@@ -17,53 +17,53 @@
     CreateContent,
     SearchBox
 ) {
-        return declare([CreateContent], {
+    return declare([CreateContent], {
 
-            postCreate: function () {
-                this.inherited(arguments);
-                // search box
-                this.own(this._searchBox = new SearchBox({}));
-                this._searchBox.placeAt(this.namePanel, "last");
-                domStyle.set(this._searchBox.domNode, "width", "auto");
-                domClass.add(this.namePanel, "epi-gadgetInnerToolbar");
-                this.own(
-                    this._searchBox.on("searchBoxChange", lang.hitch(this, this._onSearchTextChanged)),
+        postCreate: function () {
+            this.inherited(arguments);
+            // search box
+            this.own(this._searchBox = new SearchBox({}));
+            this._searchBox.placeAt(this.namePanel, "last");
+            domStyle.set(this._searchBox.domNode, "width", "auto");
+            domClass.add(this.namePanel, "epi-gadgetInnerToolbar");
+            this.own(
+                this._searchBox.on("searchBoxChange", lang.hitch(this, this._onSearchTextChanged)),
 
-                    aspect.before(this.contentTypeList, "refresh", lang.hitch(this, function () {
-                        // reset the search box and _originalGroups
-                        this._searchBox.clearValue();
-                        this._originalGroups = null;
-                    })),
+                aspect.before(this.contentTypeList, "refresh", lang.hitch(this, function () {
+                    // reset the search box and _originalGroups
+                    this._searchBox.clearValue();
+                    this._originalGroups = null;
+                })),
 
-                    aspect.after(this.contentTypeList, "setVisibility", lang.hitch(this, function (display) {
-                        if (!display) {
-                            domStyle.set(this._searchBox.domNode, "display", "none");
-                        }
-                    }), true)
-                );
-            },
-            _onSearchTextChanged: function (queryText) {
-                if (queryText) {
-                    domStyle.set(this.contentTypeList._suggestedContentTypes.domNode, "display", "none");
-                } else {
-                    domStyle.set(this.contentTypeList._suggestedContentTypes.domNode, "display", "");
-                }
-                this._originalGroups = this._originalGroups || lang.clone(this.contentTypeList.model.groups);
-                var groupKeys = Object.keys(this._originalGroups);
-
-                array.forEach(groupKeys, function (key) {
-                    var contentTypes = this._originalGroups[key].get("contentTypes");
-                    contentTypes = array.filter(contentTypes, function (item) {
-                        return item.name.toLowerCase().indexOf(queryText.toLowerCase()) !== -1 || item.localizedName.toLowerCase().indexOf(queryText.toLowerCase()) !== -1;
-                    });
-                    if (!contentTypes.length) {
-                        domStyle.set(this.contentTypeList.model.groups[key].domNode, "display", "none");
+                aspect.after(this.contentTypeList, "setVisibility", lang.hitch(this, function (display) {
+                    if (!display) {
+                        domStyle.set(this._searchBox.domNode, "display", "none");
                     }
-                    else {
-                        domStyle.set(this.contentTypeList.model.groups[key].domNode, "display", "");
-                        this.contentTypeList.model.groups[key].set("contentTypes", contentTypes);
-                    }
-                }, this);
+                }), true)
+            );
+        },
+        _onSearchTextChanged: function (queryText) {
+            if (queryText) {
+                domStyle.set(this.contentTypeList._suggestedContentTypes.domNode, "display", "none");
+            } else {
+                domStyle.set(this.contentTypeList._suggestedContentTypes.domNode, "display", "");
             }
-        });
+            this._originalGroups = this._originalGroups || lang.clone(this.contentTypeList.groups);
+            var groupKeys = Object.keys(this._originalGroups);
+
+            array.forEach(groupKeys, function (key) {
+                var contentTypes = this._originalGroups[key].get("contentTypes");
+                contentTypes = array.filter(contentTypes, function (item) {
+                    return item.name.toLowerCase().indexOf(queryText.toLowerCase()) !== -1 || item.localizedName.toLowerCase().indexOf(queryText.toLowerCase()) !== -1;
+                });
+                if (!contentTypes.length) {
+                    domStyle.set(this.contentTypeList.groups[key].domNode, "display", "none");
+                }
+                else {
+                    domStyle.set(this.contentTypeList.groups[key].domNode, "display", "");
+                    this.contentTypeList.groups[key].set("contentTypes", contentTypes);
+                }
+            }, this);
+        }
     });
+});
